ceea74d12c08987182cd65dbf5cafdd6aef52bb7
nino-it <66074076+nino-it@users.noreply.github.com>
2021-03-11 00:12:50 +0100

    Update _END.php

--- _END.php
+++ _END.php
@@ -1,13 +1,3 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="/__/firebase/8.2.10/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="/__/firebase/8.2.10/firebase-analytics.js"></script>
-
-<!-- Initialize Firebase -->
-<script src="/__/firebase/init.js"></script>
-
 <script>
 let ele = document.getElementsByTagName("sidebar");
 let blank = document.createElement("div");
